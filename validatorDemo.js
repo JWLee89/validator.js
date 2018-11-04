cc6605cec3112b273d7caa297bcc0a850cf250fb
Jay Lee <ljay189@gmail.com>
2018-11-05 08:42:00 +0900

    Update validatorDemo.js

--- validatorDemo.js
+++ validatorDemo.js
@@ -89,29 +89,6 @@ console.log("Length: ", longSentence.length, between100And200.check()); // True.
 console.log(between100And200.lessWordsThan(20).check());    // False. 단어수가 20 이상이니까
 
 /**
- * 19. 허용 브라우저 체크
- * 예: Validator.prototype.getBrowserVersion() => "Chrome 62"
- * */
-function lfn_getBrowserVersion() {
-    var ua= navigator.userAgent, tem,
-        M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if(/trident/i.test(M[1])){
-        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return 'IE '+(tem[1] || '');
-    }
-    if(M[1]=== 'Chrome'){
-        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-    }
-    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-    return M.join(' ');
-}
-
-console.log(lfn_getBrowserVersion());
-
-
-/**
  * 날짜 영역
  * =================================
  * */
